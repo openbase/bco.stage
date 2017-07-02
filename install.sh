6f06b3b7a1fc679693bce8a95b0e2d60f910d35f
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-07-03 00:56:10 +0200

    travis config files added. install script added. ACKNOWLEDGMENT added.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+APP_NAME='bco-stage'
+clear &&
+echo "=== clean ${APP_NAME} ===" &&
+mvn clean $@ &&
+clear &&
+echo "=== deploy ${APP_NAME} to ${prefix} ===" &&
+mvn install \
+        -DassembleDirectory=${prefix} \
+        -DskipTests=true \
+        -Dmaven.test.skip=true \
+        -Dlicense.skipAddThirdParty=true \
+        -Dlicense.skipUpdateProjectLicense=true \
+        -Dlicense.skipDownloadLicenses \
+        -Dlicense.skipCheckLicense=true \
+        -Dmaven.license.skip=true $@ &&
+clear &&
+echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
