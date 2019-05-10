d81d3d79e8122d400b81714efc5d463871d14a44
Divine Threepwood <divine@openbase.org>
2019-05-10 11:16:17 +0200

    fix javafx app launch by proxy main class

---
+++ src/main/java/org/openbase/bco/stage/StageLauncher.java
@@ -0,0 +1,33 @@
+package org.openbase.bco.stage;
+
+/*-
+ * #%L
+ * BCO Stage
+ * %%
+ * Copyright (C) 2017 - 2019 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+public class StageLauncher {
+
+    public static void main(String[] args) {
+        // This is the javafx workaround to probably link the javafx dependencies.
+        // Without it, the application can only be run via the javafx plugin
+        // if the main class extends the javafx application class.
+        Stage.main(args);
+    }
+}
