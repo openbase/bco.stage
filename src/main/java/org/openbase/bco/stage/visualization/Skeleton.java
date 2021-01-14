9c2636255c968a42fcd79d3c5ab1b49506f48592
Divine Threepwood <divine@openbase.org>
2021-01-14 19:34:35 +0100

    update licence header to 2021

--- src/main/java/org/openbase/bco/stage/visualization/Skeleton.java
+++ src/main/java/org/openbase/bco/stage/visualization/Skeleton.java
@@ -5,7 +5,7 @@ package org.openbase.bco.stage.visualization;
  * #%L
  * BCO Stage
  * %%
- * Copyright (C) 2017 - 2020 openbase.org
+ * Copyright (C) 2017 - 2021 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
